e837a9a948a655e5365cbc1971df3f24ed950375
Forhad Israfil <32041831+ForhadIsrafil@users.noreply.github.com>
2021-08-17 00:01:13 +0600

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -4,11 +4,11 @@ import xml
 
 class Req(object):
     def req(self):
-        url = 'https://webservice.eppo.go.th/eppowebservice/Energy_statistic.asmx?WSDL'
+        url = 'https://**.**.go.th/**/**.asmx?**'
 
         header = {'Content-Type': 'text/xml'}
 
-        body = '<?xml version="1.0" encoding="utf-8"?> <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"> <soap:Body> <GetLPGDemandSupplyMonthlyJSON xmlns="https://webservice.eppoengdb.go.th/"> <strUser>set</strUser> <strPassword>set</strPassword> <yy></yy> <mm></mm> </GetLPGDemandSupplyMonthlyJSON> </soap:Body> </soap:Envelope>'
+        body = '<?xml version="1.0" encoding="utf-8"?> <soap:Envelope xmlns:xsi="http://www.**.org/***/XMLSchema-instance" xmlns:xsd="http://www.****.org/***/XMLSchema" xmlns:soap="http://schemas.***.org/**/**/"> <soap:Body> <GetLPGDemandSupplyMonthlyJSON xmlns="https://webservice.eppoengdb.go.th/"> <strUser>set</strUser> <strPassword>set</strPassword> <yy></yy> <mm></mm> </GetLPGDemandSupplyMonthlyJSON> </soap:Body> </soap:Envelope>'
 
         res = requests.post(url, data=body, headers=header)
         print(res.status_code)
